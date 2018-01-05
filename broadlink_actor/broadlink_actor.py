79d9399f527865334aeb58f572d1b1bf6aaa2048
Carles R. Riera <blauigris@gmail.com>
2018-01-05 22:24:07 +0100

    Add state status support

--- broadlink_actor/broadlink_actor.py
+++ broadlink_actor/broadlink_actor.py
@@ -10,6 +10,14 @@ class BroadlinkActor(ActorBase):
     # custom property
     ip = Property.Text("IP", True, None)
 
+    def state(self):
+        """
+        Retrive device state
+        :return:
+        """
+        device = get_device(self.ip)
+        return device.check_power()
+
     def on(self, power=0):
         """
         Code to switch on the actor
